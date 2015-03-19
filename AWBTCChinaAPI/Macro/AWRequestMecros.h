d59cd13d6e36b5f69e8cf886d4a579681bcbf59c
wangyu <wangyu@mafengwo.com>
2015-03-19 19:02:40 +0800

    Add mecros

---
+++ AWBTCChinaAPI/Macro/AWRequestMecros.h
@@ -0,0 +1,14 @@
+//
+//  AWRequestMecros.h
+//  AWBTCChinaAPIDemo
+//
+//  Created by mafengwo on 15/3/19.
+//  Copyright (c) 2015年 Aldaron. All rights reserved.
+//
+
+#ifndef AWBTCChinaAPIDemo_AWRequestMecros_h
+#define AWBTCChinaAPIDemo_AWRequestMecros_h
+
+#define BTCCHINA_REQUEST_BASE_URL @"https://api.btcchina.com/api_trade_v1.php"
+
+#endif
